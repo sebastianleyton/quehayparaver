4670b5a95820789119e3f82993bb8282a46b73a4
Seba <sebastianleytonr@gmail.com>
2022-09-29 22:31:08 -0300

    - Redesigned database structure

--- functions/dal/db.py
+++ functions/dal/db.py
@@ -1,5 +1,6 @@
 import sqlite3
 from definitions import DB_PATH
+import datetime
 
 
 class DBConnection:
@@ -25,33 +26,56 @@ class DBConnection:
         else:
             result = self.cursor.execute(query, params)
 
-        for row in result.fetchall(): # Recorro los rows devueltos por la query
+        for row in result.fetchall():  # Recorro los rows devueltos por la query
             result_dict = {}
             for r in row.keys():
                 result_dict[r] = row[r]
             result_list.append(result_dict)
         return result_list
 
-    def get_all_pelis(self):
-        result = self.query("select * from movies")
+    def get_all_movies(self):
+        query = 'select * from movies'
+        result = self.query(query)
         return result
 
-    def insertar_peli(self, titulo, descripction):
-        tpl = (titulo, descripction)
-        query = "insert into movies (title, description) values (?, ?)"
+    def get_latest_movies(self):
+        query = 'select * from movies where created_date = (select max(created_date) from movies)'
+        result = self.query(query)
+        return result
+
+    def get_latest_movies_from_cinema(self, cinema):
+        query = 'select * from movies where cinema = ?'
+        result = self.query(query, cinema)
+        return result
+
+    def get_movie_by_id(self, movie_id):
+        query = 'select * from movies where id = ?'
+        result = self.query(query, movie_id)
+        return result
+
+    def get_movie_by_title(self, title):
+        query = 'select * from movies where title = ?'
+        result = self.query(query, title)
+        return result
+
+    def insert_movie(self, title, description, duration, genre, address, cinema, image_name):
+        timestamp = datetime.date.today()
+        tpl = (title, description, duration, genre, address, cinema, image_name, timestamp, timestamp)
+        query = 'insert into movies (title, description, duration, genre, address, cinema, image_name, created_date, ' \
+                'updated_date) values (?, ?, ?, ?, ?, ?, ? ,?, ?) '
         self.execute(query, tpl)
 
-    def insertar_muchas_pelis(self, lista_pelis):
-        for peli in lista_pelis:
-            self.insertar_peli(peli['titulo'], peli['descripcion'])
 
-    def actualizar_descripcion(self, id, nueva_descripcion):
-        tpl = (nueva_descripcion, id)
-        query = "update movies set description = ? where id = ?"
-        self.execute(query, tpl)
+def create_db_from_scratch():
+    conn = DBConnection(DB_PATH)
+    columns = 'id INTEGER PRIMARY KEY AUTOINCREMENT, title, description, duration, genre, address, cinema, ' \
+              'image_name, created_date, updated_date '
+    query = f'CREATE TABLE movies ({columns})'
+    conn.execute(query)
 
-    def actualizar_titulo(self, id, nuevo_titulo):
-        tpl = (nuevo_titulo, id)
-        query = "update movies set title = ? where id = ?"
-        self.execute(query, tpl)
+def test_db_functions():
+    db = DBConnection(DB_PATH)
+    db.insert_movie('Spiderman 3', 'Gran peli gran', '59', 'Accion', 'Complejo Ejido', 'Grupocine', 'asdasd')
+    all_data = db.get_all_movies()
+    print(all_data)
 
