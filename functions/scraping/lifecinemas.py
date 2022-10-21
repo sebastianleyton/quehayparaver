877ee44bada515244dc8687e630c14e1cf47e07e
ggutierr <ggutierrez.ucu@gmail.com>
2022-10-21 20:35:09 -0300

    moviecine.py - Testing Scraping

--- functions/scraping/lifecinemas.py
+++ functions/scraping/lifecinemas.py
@@ -3,81 +3,104 @@ import shutil
 import json
 import sys
 import requests
-
 from selenium.common import NoSuchElementException
 from definitions import MOVIE_IMAGES_PATH, JSON_PATH, DB_LIB_PATH, DB_PATH
 from scraping_helper import Scraper
 from functions.dal import db
 
-URL = "https://lifecinemas.com.uy/pelicula/cartelera"
-SELECTOR_CSS_MOVIE_LINKS = "#ultimos_estrenos .movie-container a"
-URL_BASE = "https://lifecinemas.com.uy"
-SELECTOR_FULL_MOVIE = ".movie-sucursal"
-JSON_NAME = 'life.json'
+URL = 'https://lifecinemas.com.uy/pelicula/cartelera'
+SELECTOR_CSS_MOVIE_LINKS = ".movie-container a"
+#SELECTOR_CSS_URL_MOVIE = ".movie-sucursal div.title-cont-2 h2 a"
+SELECTOR_CSS_URL_MOVIE = "div.title-cont-1 h2 a"
+SELECTOR_TITLE = '.cont-title festival h1'
+SELECTOR_DESCRIPTION = '.row h3'
+SELECTOR_IMAGEN_URL = '.img-poster'
+SELECTOR_DURATION = '.hidden-xs li:nth-child(2)'
+SELECTOR_GENERO = '.hidden-xs li:nth-child(3)'
+JSON_NAME = 'mc.json'
+
+DEBUG_FLAG = 1
+
 
 def get_links():
     nav = Scraper()
 
-    # Cargar URL
+    # Cargar URL CARTELERA DE PELIS
     nav.cargar_sitio(URL)
 
     # Guardar links de pelis
     movies_links_str = nav.extraer_url_de_lista(SELECTOR_CSS_MOVIE_LINKS)
-    # prit(movies_elements)
-
-    # Inicializar lista final de datos
-    lista_completa_de_datos = []
+    # for movies in movies_links_str:
+    #     print(movies)
+    #     nav.cargar_sitio(movies)
+    #     lista_completa_de_datos.append(nav.extraer_url(SELECTOR_CSS_URL_MOVIE))
+    #     print(lista_completa_de_datos)
+    # movies_links_str_final = nav.extraer_url_de_lista()
+    #     # Inicializar lista final de datos
+    #     lista_completa_de_datos = []
 
     # Inicar loop por cada link de peli
+    lista_completa_de_datos = []
     contador = 0
     for movie_url in movies_links_str:
+        print(movie_url)
         contador += 1
         print(f"Iterando lista: {contador} de {len(movies_links_str)}")
+        ## Caso de que es un festival
         if "/festival/" in movie_url:
             print("Ignorando url...")
             continue
+        ## Caso de que es una pelicula y hay que sacar el link final
+        elif "/pelicula/agrupacion" in movie_url:
+            lista_completa_de_datos.append(nav.extraer_url(SELECTOR_CSS_URL_MOVIE))
+        ## Caso en que la pelicula ya tiene la descripcion
+        else: lista_completa_de_datos.append(movie_url)
+        # nav.cargar_sitio(movie_url)
+        #### SCRAPING POR PELICULA
+        # try:
+        #     # Obtener titulo
+        #     titulo = nav.extraer_texto(SELECTOR_TITLE)
+        # except NoSuchElementException as e:
+        #     print(f"Excepcion al buscar titulo: {e}")
+        #     continue
+#
+#         # Crear nombre de imagen reemplazando los simbolos invalidos en windows por su equivalente textual
+#         imagen = titulo
+#         if '?' in titulo:
+#             imagen = imagen.replace('?', 'SIGNODEPREGUNTA')
+#         if ':' in titulo:
+#             imagen = imagen.replace(':', 'DOSPUNTOS')
+#
+#         # Obtener descripcion
+#         descripcion = nav.extraer_texto(SELECTOR_DESCRIPTION)
+#
+#         # Obtener URL imagen
+#         imagen_url = nav.extraer_atributo_generico(SELECTOR_IMAGEN_URL, 'src')
+#
+#         # Obtener Duracion
+#         duracion = nav.extraer_texto(SELECTOR_DURATION)
+#
+#         # Obtener Genero
+#         genero = nav.extraer_texto(SELECTOR_GENERO)
+#
+#         # Descargar Imagen
+#         download_image(imagen_url, titulo)
+#
+#         # Guardar en DB
+#         mc = db.DBConnection(DB_PATH)
+#         mc.insert_movie(titulo, descripcion, duracion, genero, 'Complejo', db.CinemecNames.moviecinema, imagen, movie_url)
+# #
 
-        nav.cargar_sitio(movie_url)
-
-        try:
-            # Obtener titulo
-            selector_titulo = SELECTOR_FULL_MOVIE + ' div.title-cont-2 h2 a'
-            print(selector_titulo)
-            titulo = nav.extraer_texto(selector_titulo)
-        except NoSuchElementException as e:
-            print(f"Excepcion al buscar titulo: {e}")
-            continue
-
-
-        # Crear nombre de imagen reemplazando los simbolos invalidos en windows por su equivalente textual
-        imagen = titulo
-        if '?' in titulo:
-            imagen = imagen.replace('?', 'SIGNODEPREGUNTA')
-        if ':' in titulo:
-            imagen = imagen.replace(':', 'DOSPUNTOS')
-
-        # Obtener descripcion
-        selector_descripcion = SELECTOR_FULL_MOVIE + ' div p'
-        descripcion = nav.extraer_texto(selector_descripcion)
-
-        # Obtener imagen url
-        selector_imagen_url = SELECTOR_FULL_MOVIE + ' div.img-movie a img'
-        imagen_url = nav.extraer_atributo_generico(selector_imagen_url, 'src')
-
-        download_image(imagen_url, titulo)
-
-        # TODO: Scrapear los campos nuevos title, description, duration, genre, address, cinema, image_name
-        # TODO: Guardar la información en la base de datos
-        dict = {"titulo": titulo, "descripcion": descripcion, "imagen": imagen}
-
-        lista_completa_de_datos.append(dict)
-
+#         dict = {"titulo": titulo, "descripcion": descripcion, "imagen": imagen}
+#         lista_completa_de_datos.append(dict)
+#
     nav.cerrar_navegador()
-    # json_object = json.dumps(lista_completa_de_datos, indent=4)
-    with open(os.path.join(JSON_PATH, JSON_NAME), "w+") as json_file:
-        json.dump(lista_completa_de_datos, json_file)
-    # return lista_completa_de_datos
-
+#       TO BE DELETED SOON
+#     # json_object = json.dumps(lista_completa_de_datos, indent=4)
+#     with open(os.path.join(JSON_PATH, JSON_NAME), "w+") as json_file:
+#         json.dump(lista_completa_de_datos, json_file)
+#     # return lista_completa_de_datos
+#
 def download_image(url, titulo, save_path=os.path.join(MOVIE_IMAGES_PATH)):
     r = requests.get(url, stream=True)
     print(r.status_code)
@@ -86,4 +109,9 @@ def download_image(url, titulo, save_path=os.path.join(MOVIE_IMAGES_PATH)):
     with open(filename, 'w+b') as f:
         shutil.copyfileobj(r.raw, f)
 
+
 get_links()
+#test2 = db.DBConnection(DB_PATH)
+#db.create_db_from_scratch()
+#all_data = test2.get_all_movies()
+#print(all_data)
